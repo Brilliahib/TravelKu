9857a01b007be7b351708b8d243af7b82543abfa
Brilliahib <brilliahib21@gmail.com>
2023-07-09 19:27:27 +0700

    add popup

--- dist/js/script.js
+++ dist/js/script.js
@@ -48,3 +48,13 @@ if (localStorage.theme === "dark" || (!("theme" in localStorage) && window.match
 } else {
   moon.contains = false;
 }
+
+function openPopup() {
+  document.getElementById("overlay").style.display = "block";
+  document.getElementById("popup").style.display = "block";
+}
+
+function closePopup() {
+  document.getElementById("overlay").style.display = "none";
+  document.getElementById("popup").style.display = "none";
+}
